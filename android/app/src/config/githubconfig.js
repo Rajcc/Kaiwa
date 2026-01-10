40364ecd1ddaa1540d2ca03f80f5260d570c40bd
Rajmukund mehta <rajmukundmehta@gmail.com>
2026-01-10 20:42:13 +0530

    cleanup:removal of sensitive files

--- android/app/src/config/githubconfig.js
+++
@@ -1,5 +0,0 @@
-export const Github_Config={
-    CLIENT_ID:'Ov23li6jeXhFzHl4bmyo',
-    REDIRECT_URL:'https://us-central1-devl-d79ce.cloudfunctions.net/githubCallback',
-    SCOPE:'repo,admin:repo_hook,user',
-}
